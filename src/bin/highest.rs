84f435d21d3c790ccbb6eb048dd2869304f06d25
Ivan Sim <ihcsim@gmail.com>
2021-07-04 15:27:13 -0700

    Fix println content
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- src/bin/highest.rs
+++ src/bin/highest.rs
@@ -1,15 +1,15 @@
 fn main() {
     let v = vec![1, 2, 3, 4, 5];
-    println!("largest: {}", highest(&v));
+    println!("highest: {}", highest(&v));
 
     let v = vec![5, 4, 3, 2, 1];
-    println!("largest: {}", highest(&v));
+    println!("highest: {}", highest(&v));
 
     let v = vec![5];
-    println!("largest: {}", highest(&v));
+    println!("highest: {}", highest(&v));
 
     let v = vec![4, 5, 2, 1, 3];
-    println!("largest: {}", highest(&v));
+    println!("highest: {}", highest(&v));
 }
 
 fn highest(v: &[i32]) -> i32 {
